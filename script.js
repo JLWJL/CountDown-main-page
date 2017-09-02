aa09e9d39dae523ac68c2dc288cb21d809738980
Junlong Wang <wjlinnz@gmail.com>
2017-09-02 21:01:33 +1200

    Add dropdown category list Add right arrow icon transform

---
+++ script.js
@@ -0,0 +1,15 @@
+
+(function(){
+	var category_btn = document.querySelector('.sr-opt-btn');
+	category_btn.addEventListener('click', function(){
+		document.querySelector('.sr-opts-list').classList.toggle('show');
+	});
+
+
+
+
+
+
+
+	
+}());
